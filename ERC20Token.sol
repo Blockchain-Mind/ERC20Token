0e9a69f8fa3e623d99827c352a7e98b74f7b588d
Shachindra <shachindra.spidey@gmail.com>
2020-12-05 02:19:58 +0530

    ERC20 Token Template

---
+++ ERC20Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.5;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Token is ERC20 {
+    /**
+    * @dev Constructor Function that gives the Sender all of the created Tokens.
+    */
+    constructor () ERC20("ERC20 Token", "ERC20") {
+        _mint(msg.sender, 100000000 * (10 ** 18));
+    }
+}
