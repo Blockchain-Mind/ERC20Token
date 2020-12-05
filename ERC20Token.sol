579c719146ea65cccf50221e90cc14e1bf2925e1
Blockchain-Mind <75337725+Blockchain-Mind@users.noreply.github.com>
2020-12-06 00:25:06 +0530

    Update ERC20Token.sol

--- ERC20Token.sol
+++ ERC20Token.sol
@@ -8,7 +8,7 @@ contract ERC20Token is ERC20 {
     /**
     * @dev Constructor Function that gives the Sender all of the created Tokens.
     */
-    constructor () ERC20("ERC20 Token", "ERC20") {
+    constructor () ERC20("Blockchain Mind", "BCM") {
         _mint(msg.sender, 100000000 * (10 ** 18));
     }
 }
