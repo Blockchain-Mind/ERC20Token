66eaa43ff2155960c6d7430dfdaa0b6c8e44c369
Blockchain-Mind <75337725+Blockchain-Mind@users.noreply.github.com>
2020-12-06 00:25:53 +0530

    Update ERC20Token_Flattened.sol

--- ERC20Token_Flattened.sol
+++ ERC20Token_Flattened.sol
@@ -553,11 +553,11 @@ contract ERC20 is Context, IERC20 {
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 }
 
-contract ERC20Token is ERC20 {
+contract BlockchainMindToken is ERC20 {
     /**
     * @dev Constructor Function that gives the Sender all of the created Tokens.
     */
-    constructor () ERC20("ERC20 Token", "ERC20") {
+    constructor () ERC20("Blockchain Mind", "BCM") {
         _mint(msg.sender, 100000000 * (10 ** 18));
     }
 }
